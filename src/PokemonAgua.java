701feb1b1879e5353227dd918599cd984a4b219a
JannC23 <jann.martinez@correounivalle.edu.co>
2025-03-25 09:24:55 -0500

    Nueva Rama

--- src/PokemonAgua.java
+++ src/PokemonAgua.java
@@ -1,3 +1,6 @@
 public class PokemonAgua extends Pokemon {
+    public PokemonAgua(String nombre, Tipo[] tipo, int puntosSalud) {
+        super(nombre, new Tipo[]{Tipo.AGUA}, puntosSalud);
+    }
     
 }
